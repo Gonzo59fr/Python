f254da860f7cad1e6ae6fb0f1ab5935d17b613f5
Gonzo59fr <45132140+Gonzo59fr@users.noreply.github.com>
2018-11-17 23:07:39 +0100

    Delete weather.py

--- weather.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-api_address='http://api.openweathermap.org/data/2.5/weather?appid=f7ae191dccb8d0627661f101a2ce2f72&q='
-city = input('City Name :')
-url = api_address + city
-json_data = requests.get(url).json()
-format_add = json_data['weather'][0]['description']
-print(format_add)
