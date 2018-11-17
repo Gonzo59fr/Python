fb74d7f363148ad2849bd59e493fde4b117040a3
Gonzo59fr <45132140+Gonzo59fr@users.noreply.github.com>
2018-11-18 00:03:29 +0100

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,8 +1,12 @@
 import requests
 
-api_address='http://api.openweathermap.org/data/2.5/weather?appid=f7ae191dccb8d0627661f101a2ce2f72&q='
+api_address='http://api.openweathermap.org/data/2.5/weather?appid=f7ae191dccb8d0627661f101a2ce2f72&units=metric&lang=fr&q='
 city = input('City Name :')
 url = api_address + city
 json_data = requests.get(url).json()
-format_add = json_data['weather'][0]['description']
-print(format_add)
+weather = json_data['weather'][0]['description']
+temperature = json_data['main']['temp']
+windspeed = json_data['wind']['speed']
+print(weather)
+print(temperature)
+print(windspeed)
